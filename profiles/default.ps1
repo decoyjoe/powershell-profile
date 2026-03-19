8bf55dec769c1988bbd5ba6cb6cdcd9ed0959cd1
Joseph Larionov <joseph@larionov.io>
2026-03-19 13:48:37 -0700

    Even more shaving

--- profiles/default.ps1
+++ profiles/default.ps1
@@ -8,7 +8,7 @@ $modulesToImport = @(
 
 foreach ($module in $modulesToImport)
 {
-    Write-Timing -Status 'BEGIN' -Message ('Import module "{0}"' -f $module)
+    # Write-Timing -Status 'BEGIN' -Message ('Import module "{0}"' -f $module)
     Import-Module -Name $module -Global
-    Write-Timing -Status 'COMPLETE' -Message ('Import module "{0}"' -f $module)
+    # Write-Timing -Status 'COMPLETE' -Message ('Import module "{0}"' -f $module)
 }
