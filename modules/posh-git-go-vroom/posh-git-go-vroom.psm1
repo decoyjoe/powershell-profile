c1be8e92cbd2b8950f67de95e26018018ff57bbb
Joseph Larionov <joseph@larionov.io>
2026-03-19 14:07:14 -0700

    My systems are always going to have git installed, so skip checking
    And if it happens to be missing, then let it throw all sorts of errors

--- modules/posh-git-go-vroom/posh-git-go-vroom.psm1
+++ modules/posh-git-go-vroom/posh-git-go-vroom.psm1
@@ -31,12 +31,6 @@ if (Test-Path Env:\POSHGIT_ENABLE_STRICTMODE) {
 # ============================================================================
 $global:GitMissing = $false
 $script:GitCygwin = $false
-
-if (!(Get-Command git -TotalCount 1 -ErrorAction SilentlyContinue)) {
-    Write-Warning "git command could not be found. Please create an alias or add it to your PATH."
-    $global:GitMissing = $true
-    return
-}
 Write-DebugTiming -Label 'CheckRequirements'
 #endregion
 
